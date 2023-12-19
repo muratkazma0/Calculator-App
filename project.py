338bf2748306db9e7082d880832f1aededff064b
Murat Kazma <muratkazma69@gmail.com>
2023-12-20 00:03:57 +0300

    Calculator App
    This Python program is a simple calculator application that allows users to perform basic arithmetic operations. The user is prompted to enter two numbers and select an operator (+, -, *, /) to perform the desired calculation. The program then displays the result of the operation.

---
+++ project.py
@@ -0,0 +1,39 @@
+# Calculator ;
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == "+":
+    result = number1 + number2
+elif operator == "-":
+    result = number1 - number2
+elif operator == "*":
+    result = number1 * number2
+elif operator == "/":
+    result = number1 / number2
+else:
+    print("Invalid operator!")
+
+print("Result:", result)
+
+try:
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+
+    if operator == "+":
+        result = number1 + number2
+    elif operator == "-":
+        result = number1 - number2
+    elif operator == "*":
+        result = number1 * number2
+    elif operator == "/":
+        result = number1 / number2
+    else:
+        print("Invalid operator!")
+
+    print("Result:", result)
+
+except ValueError:
+    print("Invalid number input!")
